76cea5b7d00d43c21f8dd1228ef81b2050acf363
Mayhem <stepien.nicolas@gmail.com>
2017-07-04 22:11:53 +0100

    Fix link failure on ARM. Close #39

--- src/_memcpy.h
+++ src/_memcpy.h
@@ -1,3 +1,6 @@
 /* force mempcy to be from earlier compatible system */
 /* see https://github.com/MayhemYDG/iltorb/issues/35 */
+/*     https://github.com/MayhemYDG/iltorb/issues/39 */
+#if defined(__linux__) && defined(__x86_64__)
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
+#endif

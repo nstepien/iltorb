b9a74caf18a9fbdabae32fd697af45f153a7a66c
Hung Tran <hung@0x0.ninja>
2018-03-13 09:12:23 -0500

    update install wrapper with comments an conditionals

--- scripts/install.js
+++ scripts/install.js
@@ -1,3 +1,15 @@
+#!/usr/bin/env node
+
+/**
+ * Modified Source: `prebuild-install`
+ * https://github.com/prebuild/prebuild-install/blob/master/bin.js
+ *
+ * This install wrapper is written to provide support for the `--no-bin-links`
+ * install option provided by npm and yarn. It takes advantage of the module
+ * resolution logic to run instead of relying on the PATH env to perform the
+ * installation.
+ */
+
 const path = require('path');
 const libc = require('detect-libc');
 const prebuildRC = require('prebuild-install/rc');
@@ -5,7 +17,26 @@ const prebuildDL = require('prebuild-install/download');
 const log = require('npmlog');
 const pkg = require(path.resolve(__dirname, '../package.json'));
 
-const opts = Object.assign({}, prebuildRC(pkg), { pkg, log });
+const conf = prebuildRC(pkg);
+const opts = Object.assign({}, conf, { pkg, log });
+
+if (conf.verbose) {
+  log.level = 'verbose';
+}
+
+if (opts.force) {
+  log.warn('install', 'prebuilt binaries enforced with --force!')
+  log.warn('install', 'prebuilt binaries may be out of date!')
+} else if (!(typeof pkg._from === 'string')) {
+  log.info('install', 'installing standalone, skipping download.')
+  process.exit(1)
+} else if (pkg._from.length > 4 && pkg._from.substr(0, 4) === 'git+') {
+  log.info('install', 'installing from git repository, skipping download.')
+  process.exit(1)
+} else if (opts.compile === true || opts.prebuild === false) {
+  log.info('install', '--build-from-source specified, not attempting download.')
+  process.exit(1)
+}
 
 prebuildDL(opts, function(err) {
   if (err) {

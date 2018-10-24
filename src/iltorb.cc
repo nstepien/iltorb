25ead225da2e45364261cbf2ebba90f551be78d6
Nicolas Stepien <stepien.nicolas@gmail.com>
2018-10-24 20:51:39 +0100

    Revert "Use NAPI_MODULE_INIT"
    This reverts commit cd07dffa80e90e5e6eef2e94d17f6d3d6f682301.
    
    NAPI_MODULE_INIT is not available for Node 6 and 8

--- src/iltorb.cc
+++ src/iltorb.cc
@@ -2,8 +2,10 @@
 #include "dec/stream_decode.h"
 #include "enc/stream_encode.h"
 
-NAPI_MODULE_INIT() {
+napi_value Init(napi_env env, napi_value exports) {
   StreamDecode::Init(env, exports);
   StreamEncode::Init(env, exports);
   return exports;
 }
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
